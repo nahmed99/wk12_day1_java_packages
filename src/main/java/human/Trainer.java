27367e83d05ccce76f50075b0f29b698f3ffa1a8
Nawaz Ahmed <ahmed_nawaz@hotmail.com>
2021-02-01 15:46:25 +0000

    created packages

--- src/main/java/human/Trainer.java
+++ src/main/java/human/Trainer.java
@@ -1,11 +1,14 @@
 package human;
 
-
 import animal.Dog;
-
+import animal.air.Bird;
 
 public class Trainer {
     public String teach(Dog dog) {
         return dog.bark();
+    }
+
+    public String trainBird(Bird bird) {
+        return bird.speak();
     }
 }
